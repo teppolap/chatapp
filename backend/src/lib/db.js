3482cc1c3c15dbb692e9197217b2dbc71cbec20f
teppolap <teppo.lappalainen28@gmail.com>
2026-01-03 17:50:00 +0200

    welcome email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import { ENV } from './env.js';
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log('MongoDB connected:', conn.connection.host);
     } catch (error) {
         console.error(error);
